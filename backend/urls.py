23ddaa76ce9d13fa2021e03483839ff7ee329726
Rodrigo Tunala Rezende <rodrigotunala@gmail.com>
2025-08-09 20:58:45 -0300

    mudança das urls

--- backend/urls.py
+++ backend/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('lista.urls')),
+    path('admin', admin.site.urls),
+    path('api', include('lista.urls')),
 
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
