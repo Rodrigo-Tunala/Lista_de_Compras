8e2f2356742a5bb454cb68090afb5864f5fe3e94
Rodrigo Tunala Rezende <rodrigotunala@gmail.com>
2025-09-20 16:53:39 -0300

    Adicionando recursos de Marca,tamanho,valor pago e quantidade.

--- lista/models.py
+++ lista/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField(default=1)
     comprado = models.BooleanField(default=False)
+
+
+    marca = models.CharField(max_length=50, blank=True, null=True)
+    tamanho = models.CharField(max_length=50, blank=True, null=True)
+    valor_pago = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
 
 
     def __str__(self):
--- lista/views.py
+++ lista/views.py
@@ -2,10 +2,24 @@ from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Item
 from .serializers import ItemSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fiels = ['comprado']
+    filterset_fields = ['comprado']
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return Item.objects.filter(comprado=False)
+
+        return Item.objects.all()
+    
+
+    @action(detail=False, methods=['get'])
+    def historico(self, request):
+        itens_comprados = Item.objects.filter(comprado=True).order_by('-atualizado_em')
+        
